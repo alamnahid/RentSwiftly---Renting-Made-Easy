460382a47daf12b7a4574e76657b5caf704107aa
alamnahid <nahidalam13765@gmail.com>
2023-12-10 15:56:06 +0600

    add images in the banner

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,10 +1,17 @@
 import BannerContents from "./BannerContents";
-
+import homeimage from "../../../assets/images/home.png"
+import dots from "../../../assets/images/dots.svg"
+import housebg from "../../../assets/images/housebg.png"
 const Banner = () => {
     return (
-        <div>
+        <div className="flex justify-evenly items-center min-h-[80vh]">
             <BannerContents/>
-            
+            <div className="relative"> 
+                <img className="" src={housebg} alt="" />
+                <img className="absolute bottom-0 right-[7rem]" src={homeimage} alt="" />
+                
+            </div>
+
             
         </div>
     );
