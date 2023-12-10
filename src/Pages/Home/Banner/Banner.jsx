5110f6c2e167d90bd4147742fb7a87f7bb7f9af0
alamnahid <nahidalam13765@gmail.com>
2023-12-10 16:27:23 +0600

    responsive the banner

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -4,11 +4,11 @@ import dots from "../../../assets/images/dots.svg"
 import housebg from "../../../assets/images/housebg.png"
 const Banner = () => {
     return (
-        <div className="flex justify-evenly items-center min-h-[80vh]">
+        <div className="flex flex-col-reverse lg:flex-row justify-evenly items-center min-h-[80vh]">
             <BannerContents/>
-            <div className="relative"> 
-                <img className="" src={housebg} alt="" />
-                <img className="absolute bottom-0 right-[7rem]" src={homeimage} alt="" />
+            <div className="relative w-[300px] md:w-auto"> 
+                <img className="md:w-auto" src={housebg} alt="" />
+                <img className="absolute bottom-0 right-16 md:right-[7rem]" src={homeimage} alt="" />
                 
             </div>
 
--- src/Pages/Home/Banner/BannerContents.jsx
+++ src/Pages/Home/Banner/BannerContents.jsx
@@ -3,15 +3,15 @@ import searchicon from "../../../assets/icons/search icon.svg"
 
 const BannerContents = () => {
     return (
-        <div>
-            <h1 className="text-[#091638] lg:text-[4rem] font-semibold lg:w-[35rem] merrin">Modern living for everyone</h1>
+        <div className="px-4 lg:px-0">
+            <h1 className="text-[#091638] text-2xl lg:text-[4rem] font-semibold lg:w-[35rem] merrin">Modern living for everyone</h1>
 
-            <p className="text-[#091638] sans w-[37rem] text-[1.32rem]">We provide a complete service for the sale, purchase or rental of real estate. We have been operating in Madrid and Barcelona more than 15 years.</p>
+            <p className="text-[#091638] sans lg:w-[37rem] lg:text-[1.32rem]">We provide a complete service for the sale, purchase or rental of real estate. We have been operating in Madrid and Barcelona more than 15 years.</p>
 
-            <div className="w-[33rem] h-[6.5rem] rounded-xl bg-[#F3F3FA] mt-6 flex justify-center items-center">
+            <div className="lg:w-[33rem] h-[6.5rem] rounded-xl bg-[#F3F3FA] mt-6 flex justify-center items-center">
 
-               <div className="w-[19rem] h-[3.5rem] relative">
-               <input className="w-[19rem] border-none outline-none h-[3.5rem] text-lg pl-10 bg-white shadow-sm rounded-sm" type="text" placeholder="Search from location" />
+               <div className="md:w-[19rem] h-[3.5rem] relative">
+               <input className="md:w-[19rem] border-none outline-none h-[3.5rem] text-lg pl-10 bg-white shadow-sm rounded-sm" type="text" placeholder="Search from location" />
                <img className="absolute top-[1rem] left-4" src={location} alt="" />
                </div>
 
