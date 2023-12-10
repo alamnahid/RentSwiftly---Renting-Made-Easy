73b56e236c56987a7eb17407e9c10fd9da221811
alamnahid <nahidalam13765@gmail.com>
2023-12-10 18:41:11 +0600

    responsive about us section

--- src/Pages/Home/AboutUs/AboutUs.jsx
+++ src/Pages/Home/AboutUs/AboutUs.jsx
@@ -3,7 +3,7 @@ import AboutUsContent from "./AboutUsContent";
 
 const AboutUs = () => {
     return (
-        <div className="flex justify-evenly items-center">
+        <div className="flex px-4 lg:px-0 flex-col lg:flex-row justify-evenly items-center">
             <img src={aboutusimage} alt="" />
             <AboutUsContent/>
         </div>
