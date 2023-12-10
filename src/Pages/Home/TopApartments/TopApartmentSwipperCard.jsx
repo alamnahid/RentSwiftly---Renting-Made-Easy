e0df0594117ad88065db73cf865f8ff7915817d6
alamnahid <nahidalam13765@gmail.com>
2023-12-10 19:27:31 +0600

    complete the swipper

--- src/Pages/Home/TopApartments/TopApartmentSwipperCard.jsx
+++ src/Pages/Home/TopApartments/TopApartmentSwipperCard.jsx
@@ -2,7 +2,7 @@ import image from "../../../assets/images/cardimage.png"
 
 const TopApartmentSwipperCard = () => {
     return (
-        <div className="w-[21.875rem] h-[26.25rem] rounded-md shadow-md hover:shadow-2xl hover:shadow-[#1C3988] bg-white">
+        <div className="w-[21.875rem] h-[26.25rem] rounded-md shadow-md hover:shadow-2xl hover:shadow-[#1C3988] hover:cursor-grab bg-white">
             <img className="w-[21.875rem] rounded-t-md h-[15.5625rem]" src={image} alt="" />
             <h1 className="text-[#141B2D] text-[1.32rem] font-semibold px-5 mt-6 merrin">Large 4-room apartment with a beautiful terrace</h1>
 
--- src/Pages/Home/TopApartments/TopApartments.jsx
+++ src/Pages/Home/TopApartments/TopApartments.jsx
@@ -8,58 +8,60 @@ import TopApartmentSwipperCard from './TopApartmentSwipperCard';
 
 const TopApartments = () => {
     return (
-        <div className="h-[57rem] bg-[#F3F3FA]">
+        <div className="h-[57rem] bg-[#F3F3FA] pt-[7.5rem]">
+            <div className='pl-[10%]'>
             <h1 className="text-[#091638] merrin text-[3.2rem] font-semibold">Top Apartments</h1>
             <p className="text-[#091638] text-[1.2rem] w-[38rem] sans">Fulfill your career dreams, enjoy all the achievements of the city center and luxury housing to the fullest.</p>
 
             <div className="flex">
-            <div className="divider w-[80vw]"></div> 
-            <button className="btn btn-outline border-[#1C3988] ml-4 text-lg border-2">Show All Apartment</button>
+                <div className="divider w-[70vw]"></div>
+                <button className="btn btn-outline border-[#1C3988] ml-4 text-lg border-2">Show All Apartment</button>
+            </div>
             </div>
 
-            <div className='mt-20 px-[8%]'>
-           
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={30}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Pagination]}
-                className="mySwiper mt-12"
-                autoplay
-                breakpoints={{
-                    320: {
-                      slidesPerView: 1, // 1 slide per view on small screens (mobile devices)
-                    },
-                    768: {
-                      slidesPerView: 1.5, // 2 slides per view on tablets
-                    },
-                    1024: {
-                      slidesPerView: 4, // 4 slides per view on desktop devices
-                    },
-                  }}
+            <div className='mt-20 pl-[10%]'>
 
-            >
-                <SwiperSlide  className='pb-12'>
-                   <TopApartmentSwipperCard/>
-                </SwiperSlide>
-                <SwiperSlide  className='pb-12'>
-                    <h1>nahid alam</h1>
-                </SwiperSlide>
-                <SwiperSlide  className='pb-12'>
-                    <h1>nahid alam</h1>
-                </SwiperSlide>
-                <SwiperSlide  className='pb-12'>
-                    <h1>nahid alam</h1>
-                </SwiperSlide>
-                <SwiperSlide  className='pb-12'>
-                    <h1>nahid alam</h1>
-                </SwiperSlide>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper mt-12"
+                    autoplay
+                    breakpoints={{
+                        320: {
+                            slidesPerView: 1, // 1 slide per view on small screens (mobile devices)
+                        },
+                        768: {
+                            slidesPerView: 1.5, // 2 slides per view on tablets
+                        },
+                        1024: {
+                            slidesPerView: 4.5, // 4 slides per view on desktop devices
+                        },
+                    }}
 
-            </Swiper>
+                >
+                    <SwiperSlide className='pb-12'>
+                        <TopApartmentSwipperCard />
+                    </SwiperSlide>
+                    <SwiperSlide className='pb-12'>
+                        <TopApartmentSwipperCard />
+                    </SwiperSlide>
+                    <SwiperSlide className='pb-12'>
+                        <TopApartmentSwipperCard />
+                    </SwiperSlide>
+                    <SwiperSlide className='pb-12'>
+                        <TopApartmentSwipperCard />
+                    </SwiperSlide>
+                    <SwiperSlide className='pb-12'>
+                        <TopApartmentSwipperCard />
+                    </SwiperSlide>
 
-        </div>
+                </Swiper>
+
+            </div>
         </div>
     );
 };
