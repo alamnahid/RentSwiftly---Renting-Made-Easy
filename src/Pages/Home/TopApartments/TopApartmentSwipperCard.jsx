9b3dfe9d020de48b684805c398f789be0dd16042
alamnahid <nahidalam13765@gmail.com>
2023-12-10 19:22:01 +0600

    create a card for swipper

--- src/Pages/Home/TopApartments/TopApartmentSwipperCard.jsx
+++ src/Pages/Home/TopApartments/TopApartmentSwipperCard.jsx
@@ -2,10 +2,18 @@ import image from "../../../assets/images/cardimage.png"
 
 const TopApartmentSwipperCard = () => {
     return (
-        <div className="w-[21.875rem] h-[26.25rem] rounded bg-white">
-            <img className="w-[21.875rem] h-[15.5625rem]" src={image} alt="" />
-            <h1>Large 4-room apartment with a beautiful terrace</h1>
-            
+        <div className="w-[21.875rem] h-[26.25rem] rounded-md shadow-md hover:shadow-2xl hover:shadow-[#1C3988] bg-white">
+            <img className="w-[21.875rem] rounded-t-md h-[15.5625rem]" src={image} alt="" />
+            <h1 className="text-[#141B2D] text-[1.32rem] font-semibold px-5 mt-6 merrin">Large 4-room apartment with a beautiful terrace</h1>
+
+            <div className="flex mt-4 justify-between items-center pr-5">
+                <div>
+                    <p className="text-[#1C3988] text-lg sans font-semibold pl-5">$320</p>
+                    <p className=" text-[#141B2D] sans pl-5">Daka, Bangladesh</p>
+                </div>
+                <button className="btn btn-outline border-[#1C3988] text-lg border-2">See Details</button>
+            </div>
+
         </div>
     );
 };
