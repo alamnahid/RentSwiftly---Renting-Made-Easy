9bae274337d373f146cda1c91d3c6a30832de707
alamnahid <nahidalam13765@gmail.com>
2023-12-15 11:58:56 +0600

    create a card for show the availabe houses

--- src/Pages/AvailableHouse/AvailableHouse.jsx
+++ src/Pages/AvailableHouse/AvailableHouse.jsx
@@ -1,9 +1,26 @@
 
-
+import image from "../../assets/images/cardimage.png"
 const AvailableHouse = () => {
     return (
-        <div>
-            
+        <div className="mx-[10%] mt-20">
+
+
+            <div className="w-[21.875rem] h-[26.25rem] rounded-tr-[3rem] shadow-md hover:shadow-2xl hover:shadow-[#1C3988] hover:cursor-grab bg-white">
+                <img className="w-[21.875rem] rounded-tr-[3rem] h-[15.5625rem]" src={image} alt="" />
+                <h1 className="text-[#141B2D] text-[1.32rem] font-semibold px-5 mt-6 merrin">Large 4-room apartment with a beautiful terrace</h1>
+
+                <div className="flex mt-4 justify-between items-center pr-5">
+                    <div>
+                        <p className="text-[#1C3988] text-lg sans font-semibold pl-5">$320</p>
+                        <p className=" text-[#141B2D] sans pl-5">Daka, Bangladesh</p>
+                    </div>
+                    <button className="btn btn-outline border-[#1C3988] text-lg border-2">See Details</button>
+                </div>
+
+            </div>
+
+
+
         </div>
     );
 };
