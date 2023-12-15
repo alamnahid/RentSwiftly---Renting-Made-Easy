6e63d15114bf21cc3a44209aea82409e16077cd2
alamnahid <nahidalam13765@gmail.com>
2023-12-15 11:52:42 +0600

    complete the service area section

--- src/Pages/Home/GoogleMap/GoogleMapForService.jsx
+++ src/Pages/Home/GoogleMap/GoogleMapForService.jsx
@@ -2,8 +2,14 @@
 
 const GoogleMapForService = () => {
     return (
+        <div className="mx-[10%] my-20 flex flex-col lg:flex-row justify-evenly  gap-4">
+
         <div>
-            <div className="overflow-hidden max-w-full w-full md:max-w-[1000px] h-[500px]">
+        <h1 className="text-[#091638] merrin text-2xl lg:text-[3.2rem] font-semibold">Service Area</h1>
+            <p className="text-[#091638] mt-8 lg:text-[1.2rem] sans">Explore our expansive service area on the interactive map below, designed to seamlessly guide you through the locations we proudly serve. From city centers to suburban retreats, our commitment to providing exceptional service extends far and wide.</p>
+        </div>
+
+            <div className="overflow-hidden hover:shadow-lg hover:shadow-[#1C3988] max-w-full w-full md:min-w-[50vw] h-[600px]">
 
                 <div id="gmap-canvas" className="h-full w-full max-w-full">
 
